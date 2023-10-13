01f8a5bd0ced4b2e7b98f19ac01745c6904152c3
LeeGa-ye <144295611+LeeGa-ye@users.noreply.github.com>
2023-10-13 11:41:53 +0900

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello,World")
+Hello,World
+"Hello,World"
+'Hello,World'
+Hello,World
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    Hello,World
+NameError: name 'Hello' is not defined
+Hello World
+SyntaxError: incomplete input
+4+4
+8
